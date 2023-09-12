df9299f1d3d0c4e2b6dad5e4c41afc9c505926fa
Brandon Duong <bduong1497@gmail.com>
2023-09-12 01:55:18 -0400

    fixed a bug

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-const container = document.getElementById("grid-container");
-const div = document.createElement("div");
-// Loop through rows and columns to create the cells
-for (let row = 0; row < 16; row++) {
-  for (let col = 0; col < 16; col++) {
-    // Create a new cell element
-    const cell = document.createElement("div");
-    cell.classList.add("grid-cell");
-    container.appendChild(cell);
+function createGrid(rows, columns) {
+  const container = document.getElementById("grid-container");
+  // Loop through rows and columns to create the cells
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < columns; col++) {
+      // Create a new cell element
+      const cell = document.createElement("div");
+      cell.classList.add("grid-cell");
+      container.appendChild(cell);
+    }
   }
 }
+
+createGrid(16, 16);
